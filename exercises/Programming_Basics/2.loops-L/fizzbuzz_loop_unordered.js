38c0bdfb607dbc6a097ccf140ebe05297652a302
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-02-24 14:40:57 +0100

    add temp loop exercises

---
+++ exercises/Programming_Basics/2.loops-L/fizzbuzz_loop_unordered.js
@@ -0,0 +1,12 @@
+currentNum__
+console.log('FizzBuzz')
+else
+else if (currentNum _ 3 === 0)
+if (currentNum % _ === 0 && currentNum % _ === 0)
+console.log(`${__}`)
+console.log('Buzz')
+console.log(`Fizz`)
+var endNum = ___
+while (__ <= __)
+var currentNum = _
+else if (__ % 5 === 0)
