6f44951592ec90813b87a3c970a114afc8a6c12f
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-03-02 16:18:55 +0100

    create toggle update

--- exercises/Browser/VanillaJSDOM/DOM-1.createToggle/createToggle.js
+++ exercises/Browser/VanillaJSDOM/DOM-1.createToggle/createToggle.js
@@ -1,18 +1,16 @@
 let button = document.querySelector('button');
 let list = document.querySelector('ul');
-let paragraph = document.querySelector('p');
-let secondButton = document.querySelector('button:nth-of-type(2)');
 
-let listVisible = true;
+let listVisible = false;
 let paragraphIsShown = true;
 
 button.addEventListener('click', function() {
+  listVisible = !listVisible;
   if (listVisible) {
-    list.style.display = 'none';
-    button.innerHTML = 'Show favorite destinations';
-  } else {
     list.style.display = 'block';
     button.innerHTML = 'Get them out of here!';
+  } else {
+    list.style.display = 'none';
+    button.innerHTML = 'Show favorite destinations';
   }
-  listVisible = !listVisible;
 })
